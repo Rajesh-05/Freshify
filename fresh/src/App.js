351a588e222a518e1ccff3c34df49bdd3a73dbb3
JAIGURU <jaiguru.l2022ai-ml@sece.ac.in>
2024-04-21 11:25:29 +0530

    added basename

--- fresh/src/App.js
+++ fresh/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className="App">
     <Router >
-      <Routes basename="/">
+      <Routes basename="/Extraterrestrial.git">
         <Route path="/" element={<Main />} />
         <Route path="/home" element={<Home />} />
       </Routes>
